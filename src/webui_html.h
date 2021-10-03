6ad4870b285bb26f8ea27c3c45dfc47cc87eda7e
Matthew Kendall <mdkendall@users.noreply.github.com>
2021-10-03 16:42:28 -0700

    Formatting

--- src/webui_html.h
+++ src/webui_html.h
@@ -1,20 +1,24 @@
 
-static const char dashboardHtml[] =
-"<div id='app'>"
-"<h2>Rotator</h2>"
-"<div>Azimuth: {{info.rotator.az}}<br/>Elevation: {{info.rotator.el}}</div>"
-"</div>";
+static const char dashboardHtml[] = R"EOF(
+<div id='app'>
+    <h2>Rotator</h2>
+    <div>Azimuth: {{info.rotator.az}}<br/>Elevation: {{info.rotator.el}}</div>
+</div>
+)EOF";
 
-static const char dashboardScript[] =
-    "<script>"
-    "  var app = new Vue({"
-    "    el: '#app',"
-    "    data: {"
-    "      info: null,"
-    "      timer: ''"
-    "    },"
-    "    mounted () {"
-    "      this.timer = setInterval(function() { axios.get('/api').then(response => (this.info = response.data)) }.bind(this), 1000);"
-    "    }"
-    "  })"
-    "</script>";
+static const char dashboardScript[] = R"EOF(
+<script>
+    var app = new Vue({
+        el: '#app',
+        data: {
+            info: null,
+            timer: ''
+        },
+        mounted () {
+            this.timer = setInterval(function() {
+                axios.get('/api').then(response => (this.info = response.data))
+            }.bind(this), 1000);
+        }
+    });
+</script>
+)EOF";
